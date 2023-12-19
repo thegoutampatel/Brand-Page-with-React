c241b1b947df7f45548c9b11eeccb7d4f0c38f41
thegoutampatel <Goutampatel9131@gmail.com>
2023-12-19 15:36:26 +0530

    adding Tailwind css

---
+++ src/BrandPageIndex.jsx
@@ -0,0 +1,17 @@
+import './BrandPageIndex.css';
+export default function BrandPageIndex(){
+    return(
+        <div className=''>
+            <nav>
+                <img src="" alt="" />
+                <div>
+                    <a href="">MENU</a>
+                    <a href="">LOCATION</a>
+                    <a href="">ABOUT</a>
+                    <a href="">CONTACT</a>
+                </div>
+                <button>Login</button>
+            </nav>
+        </div>
+    )
+}
